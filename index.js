6ef3b20549ac6488488887c40b981f2ab12c3762
Muhammad Shehroz <bilal.shehroz420@gmail.com>
2024-10-22 20:33:25 +0500

    s

--- index.js
+++ index.js
@@ -122,7 +122,7 @@ app.get('/api/user', (req, res) => {
 
   if (!token) return res.status(401).json({ message: 'No token provided' });
 
-  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+  jwt.verify(token, "somececret", (err, decoded) => {
     if (err) return res.status(403).json({ message: 'Failed to authenticate token' });
 
     // Find the user based on decoded token data
