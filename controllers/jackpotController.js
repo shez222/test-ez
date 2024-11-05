7a274d203653212f4914dfbc070be8b13479a124
= <Muhammd.Shehroz@fissionfox.com>
2024-11-05 21:44:13 +0500

    change

--- controllers/jackpotController.js
+++ controllers/jackpotController.js
@@ -181,17 +181,17 @@ const joinJackpot = async (req, res) => {
     // console.log(manager);
       // console.log(jackpot);
       
-      // const tradeData = await sendTradeOfferToUser(tradeUrl,items);
+      const tradeData = await sendTradeOfferToUser(tradeUrl,items);
 
       // Send trade URL to user
       // console.log(tradeData);
-      await addUserToJackpot(userId, itemIds, jackpot._id);
+      // await addUserToJackpot(userId, itemIds, jackpot._id);
       res.json({
         success: true,
         message: 'Trade offer sent. Please accept the offer to join the jackpot.',
-        // tradeOfferUrl: tradeData.offerUrl,
+        tradeOfferUrl: tradeData.offerUrl,
       });
-      // trackTradeOffer(tradeData.offerId, userId, itemIds, jackpot._id);
+      trackTradeOffer(tradeData.offerId, userId, itemIds, jackpot._id);
       
       
       // Track trade offer acceptance
--- steamTradeBot.js
+++ steamTradeBot.js
@@ -1,8 +1,90 @@
+const SteamUser = require('steam-user');
+const SteamCommunity = require('steamcommunity');
+const SteamTradeManager = require('steam-tradeoffer-manager');
+const SteamTotp = require('steam-totp');
+const fs = require('fs');
+
+const client = new SteamUser();
+const community = new SteamCommunity();
+const manager = new SteamTradeManager({
+  steam: client,
+  community: community,
+  language: 'en',
+  useAccessToken: true
+});
+
+// Steam bot credentials from environment variables
+const config = {
+  accountName: process.env.STEAM_ACCOUNT_NAME,
+  password: process.env.STEAM_PASSWORD,
+  sharedSecret: process.env.STEAM_SHARED_SECRET,
+  identitySecret: process.env.STEAM_IDENTITY_SECRET
+};
+
+console.log('Config:', config);
+
+// Validate Steam credentials
+if (!config.accountName || !config.password || !config.sharedSecret || !config.identitySecret) {
+  console.error('Steam credentials are not fully set in environment variables.');
+  process.exit(1);
+}
+
+// Function to log in to Steam
+function loginToSteam() {
+  client.logOn({
+    accountName: config.accountName,
+    password: config.password,
+    twoFactorCode: SteamTotp.generateAuthCode(config.sharedSecret)
+  });
+}
+
+// Log in to Steam initially
+loginToSteam();
+
+client.on('loggedOn', () => {
+  client.setPersona(SteamUser.EPersonaState.Online);
+  client.gamesPlayed([252490]); // Example game ID
+  console.log('Steam client logged in and online');
+});
+
+// Error handler
+client.on('error', (err) => {
+  console.error('Steam client encountered an error:', err);
+  // Attempt to re-log after error if logged off
+  if (err.eresult === SteamUser.EResult.LoggedOff || err.eresult === SteamUser.EResult.NoConnection) {
+    setTimeout(loginToSteam, 5000); // Try re-logging in after 5 seconds
+  }
+});
+
+client.on('disconnected', (eresult, msg) => {
+  console.log(`Disconnected from Steam (${eresult}): ${msg}. Attempting to relog.`);
+  setTimeout(loginToSteam, 5000); // Attempt re-login after 5 seconds
+});
+
+client.on('webSession', (sessionId, cookies) => {
+  console.log('Web session established.');
+  manager.setCookies(cookies);
+  community.setCookies(cookies);
+  community.startConfirmationChecker(20000, config.identitySecret);
+});
+
+module.exports = { manager };
+
+
+
+
+
+
+
+
+
+
+
+
 // const SteamUser = require('steam-user');
 // const SteamCommunity = require('steamcommunity');
 // const SteamTradeManager = require('steam-tradeoffer-manager');
 // const SteamTotp = require('steam-totp');
-// const fs = require('fs');
 
 // const client = new SteamUser();
 // const community = new SteamCommunity();
@@ -11,6 +93,7 @@
 //   community: community,
 //   language: 'en',
 //   useAccessToken: true
+  
 // });
 
 // // Steam bot credentials from environment variables
@@ -29,17 +112,12 @@
 //   process.exit(1);
 // }
 
-// // Function to log in to Steam
-// function loginToSteam() {
-//   client.logOn({
-//     accountName: config.accountName,
-//     password: config.password,
-//     twoFactorCode: SteamTotp.generateAuthCode(config.sharedSecret)
-//   });
-// }
-
-// // Log in to Steam initially
-// loginToSteam();
+// // Log in to Steam
+// client.logOn({
+//   accountName: config.accountName,
+//   password: config.password,
+//   twoFactorCode: SteamTotp.generateAuthCode(config.sharedSecret)
+// });
 
 // client.on('loggedOn', () => {
 //   client.setPersona(SteamUser.EPersonaState.Online);
@@ -47,18 +125,8 @@
 //   console.log('Steam client logged in and online');
 // });
 
-// // Error handler
 // client.on('error', (err) => {
 //   console.error('Steam client encountered an error:', err);
-//   // Attempt to re-log after error if logged off
-//   if (err.eresult === SteamUser.EResult.LoggedOff || err.eresult === SteamUser.EResult.NoConnection) {
-//     setTimeout(loginToSteam, 5000); // Try re-logging in after 5 seconds
-//   }
-// });
-
-// client.on('disconnected', (eresult, msg) => {
-//   console.log(`Disconnected from Steam (${eresult}): ${msg}. Attempting to relog.`);
-//   setTimeout(loginToSteam, 5000); // Attempt re-login after 5 seconds
 // });
 
 // client.on('webSession', (sessionId, cookies) => {
@@ -69,71 +137,3 @@
 // });
 
 // module.exports = { manager };
-
-
-
-
-
-
-
-
-
-
-
-
-// // const SteamUser = require('steam-user');
-// // const SteamCommunity = require('steamcommunity');
-// // const SteamTradeManager = require('steam-tradeoffer-manager');
-// // const SteamTotp = require('steam-totp');
-
-// // const client = new SteamUser();
-// // const community = new SteamCommunity();
-// // const manager = new SteamTradeManager({
-// //   steam: client,
-// //   community: community,
-// //   language: 'en',
-// //   useAccessToken: true
-  
-// // });
-
-// // // Steam bot credentials from environment variables
-// // const config = {
-// //   accountName: process.env.STEAM_ACCOUNT_NAME,
-// //   password: process.env.STEAM_PASSWORD,
-// //   sharedSecret: process.env.STEAM_SHARED_SECRET,
-// //   identitySecret: process.env.STEAM_IDENTITY_SECRET
-// // };
-
-// // console.log('Config:', config);
-
-// // // Validate Steam credentials
-// // if (!config.accountName || !config.password || !config.sharedSecret || !config.identitySecret) {
-// //   console.error('Steam credentials are not fully set in environment variables.');
-// //   process.exit(1);
-// // }
-
-// // // Log in to Steam
-// // client.logOn({
-// //   accountName: config.accountName,
-// //   password: config.password,
-// //   twoFactorCode: SteamTotp.generateAuthCode(config.sharedSecret)
-// // });
-
-// // client.on('loggedOn', () => {
-// //   client.setPersona(SteamUser.EPersonaState.Online);
-// //   client.gamesPlayed([252490]); // Example game ID
-// //   console.log('Steam client logged in and online');
-// // });
-
-// // client.on('error', (err) => {
-// //   console.error('Steam client encountered an error:', err);
-// // });
-
-// // client.on('webSession', (sessionId, cookies) => {
-// //   console.log('Web session established.');
-// //   manager.setCookies(cookies);
-// //   community.setCookies(cookies);
-// //   community.startConfirmationChecker(20000, config.identitySecret);
-// // });
-
-// // module.exports = { manager };
